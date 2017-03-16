f08621fc73a314abac54a0c025e47eeb38f0b9f2
Elsa Nyrhinen <elsa.nyrhinen@helsinki.fi>
2017-03-16 18:00:13 +0200

    nb projekti lisätty

---
+++ Opintonaattori/src/main/java/com/opintonaattori/Main.java
@@ -0,0 +1,14 @@
+
+package com.opintonaattori;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int a = 0;
+        System.out.println(a);
+    }
+    
+    public static boolean testi() {
+        return 2>1;
+    }
+}
